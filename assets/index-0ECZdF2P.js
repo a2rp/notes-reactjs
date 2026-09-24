2ce2a212eb8ce332d6ecf5e177403be1ce99e6f2
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-09-24 15:46:27 +0530

    Updates

---
+++ assets/index-0ECZdF2P.js
@@ -0,0 +1,72 @@
+import{d as i,j as e,G as d,F as o,a as c,M as h,b as x,c as p,e as j,f as m}from"./index-CG9onkfb.js";const f={Wrapper:i.div`
+        padding: 24px;
+
+        h1 {
+            margin-bottom: 30px;
+        }
+
+        fieldset {
+            padding: 15px;
+            margin: 50px 0;
+            border: 1px solid #333;
+
+            legend {
+                padding: 0 15px;
+                font-size: 16px;
+                color: #64493d;
+            }
+            .para {
+                display: block;
+                margin-bottom: 15px;
+                max-width: 900px;
+
+                .heading {
+                    margin-bottom: 15px;
+                }
+                p {
+                    margin-bottom: 15px;
+                }
+            }
+        }
+    `},a=i.div`
+    /* border-bottom: 1px solid #ccc; */
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 15px;
+
+    &:hover {
+        background-color: #222;
+        /* color: #000;
+        a {
+            color: #000;
+        } */
+    }
+`,n=i.div`
+    flex: 0 0 80px;
+`,s=i.div`
+    /* flex: 1 1 100%; */
+    /* border: 1px solid #f00; */
+    display: flex;
+    align-items: center;
+    gap: 15px;
+
+    a {
+        color: #aaa;
+        overflow-wrap: anywhere;
+        word-break: break-word;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+    .icon {
+        /* border: 1px solid #f00; */
+        width: 50px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+`,g=({width:r=24,height:l=24})=>e.jsxs("svg",{width:r,height:l,viewBox:"0 0 3 2",xmlns:"http://www.w3.org/2000/svg",children:[e.jsx("path",{d:"M0,0 H3 V2 H0 Z",fill:"#f93"}),e.jsx("path",{d:"M0,0.667 H3 V1.333 H0 Z",fill:"#fff"}),e.jsx("path",{d:"M0,1.333 H3 V2 H0 Z",fill:"#128807"}),e.jsx("circle",{cx:"1.5",cy:"1",r:"0.2",fill:"#008"})]});function t(r){return d({attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4"},child:[]},{tag:"path",attr:{d:"M11.5 3a16.989 16.989 0 0 0 -1.826 4"},child:[]},{tag:"path",attr:{d:"M12.5 3a16.989 16.989 0 0 1 1.828 4"},child:[]},{tag:"path",attr:{d:"M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4"},child:[]},{tag:"path",attr:{d:"M11.5 21a16.989 16.989 0 0 1 -1.826 -4"},child:[]},{tag:"path",attr:{d:"M12.5 21a16.989 16.989 0 0 0 1.828 -4"},child:[]},{tag:"path",attr:{d:"M2 10l1 4l1.5 -4l1.5 4l1 -4"},child:[]},{tag:"path",attr:{d:"M17 10l1 4l1.5 -4l1.5 4l1 -4"},child:[]},{tag:"path",attr:{d:"M9.5 10l1 4l1.5 -4l1.5 4l1 -4"},child:[]}]})(r)}const w=()=>e.jsx(e.Fragment,{children:e.jsxs(f.Wrapper,{children:[e.jsx("h3",{children:"ReactJS Notes - last updated: Sep 19, 2025"}),e.jsxs("fieldset",{children:[e.jsx("legend",{children:"About Project"}),e.jsxs("div",{className:"para",children:[e.jsx("h2",{className:"heading",children:"From Wikipedia, the free encyclopedia"}),e.jsx("p",{children:'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library that aims to make building user interfaces based on components more "seamless". It is maintained by Meta (formerly Facebook) and a community of individual developers and companies.'}),e.jsx("p",{children:"React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js and Remix. Because React is only concerned with the user interface and rendering components to the DOM, React applications often rely on libraries for routing and other client-side functionality. A key advantage of React is that it only re-renders those parts of the page that have changed, avoiding unnecessary re-rendering of unchanged DOM elements."})]})]}),e.jsxs("fieldset",{children:[e.jsx("legend",{children:"About Developer"}),e.jsxs("div",{className:"aboutDeveloper",children:[e.jsxs(a,{children:[e.jsx(n,{children:"Name"}),e.jsxs(s,{children:["Ashish Ranjan",e.jsx("div",{className:"icon",children:e.jsx(o,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Phone"}),e.jsxs(s,{children:[e.jsx("a",{href:"tel:+918123747965",children:"+91 8123747965"}),e.jsx("div",{className:"icon",children:e.jsx(c,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Email"}),e.jsxs(s,{children:[e.jsx("a",{href:"mailto:ash.ranjan09@gmail.com",children:"ash.ranjan09@gmail.com"}),e.jsx("div",{className:"icon",children:e.jsx(h,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Nationality"}),e.jsxs(s,{children:["The Republic of India",e.jsx("div",{className:"icon",children:e.jsx(g,{})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Website"}),e.jsxs(s,{children:[e.jsx("a",{href:"https://www.ashishranjan.net/",target:"_blank",rel:"noopener noreferrer",children:"https://www.ashishranjan.net/"}),e.jsx("div",{className:"icon",children:e.jsx(t,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Old Website"}),e.jsxs(s,{children:[e.jsx("a",{href:"http://www.ashishranjan.in/",target:"_blank",rel:"noopener noreferrer",children:"http://www.ashishranjan.in/"}),e.jsx("div",{className:"icon",children:e.jsx(t,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"Facebook"}),e.jsxs(s,{children:[e.jsx("a",{href:"https://www.facebook.com/theash.ashish/",target:"_blank",rel:"noopener noreferrer",children:"facebook.com/theash.ashish/"}),e.jsx("div",{className:"icon",children:e.jsx(x,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"LinkedIn"}),e.jsxs(s,{children:[e.jsx("a",{href:"https://www.linkedin.com/in/aashishranjan/",target:"_blank",rel:"noopener noreferrer",children:"linkedin.com/in/aashishranjan/"}),e.jsx("div",{className:"icon",children:e.jsx(p,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"YouTube"}),e.jsxs(s,{children:[e.jsx("a",{href:"https://www.youtube.com/channel/UCLHIBQeFQIxmRveVAjLvlbQ",target:"_blank",rel:"noopener noreferrer",children:"youtube.com/channel/UCLHIBQeFQIxmRveVAjLvlbQ"}),e.jsx("div",{className:"icon",children:e.jsx(j,{size:20})})]})]}),e.jsxs(a,{children:[e.jsx(n,{children:"GitHub"}),e.jsxs(s,{children:[e.jsx("a",{href:"https://github.com/a2rp",target:"_blank",rel:"noopener noreferrer",children:"github.com/a2rp"}),e.jsx("div",{className:"icon",children:e.jsx(m,{size:20})})]})]})]})]})]})});export{w as default};
---
+++ assets/index-khf04PP-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CG9onkfb.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
